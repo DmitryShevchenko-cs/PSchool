f2380b795e8475e483b53d403d205075a64fbc75
Dmitry <dima.shevchenko10@gmail.com>
2024-08-14 01:53:33 +0300

    change to collection

--- PSchool.DAL/Entities/Parent.cs
+++ PSchool.DAL/Entities/Parent.cs
@@ -6,5 +6,5 @@ public class Parent : BaseEntity
     public string SecondName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
-    public IEnumerable<Student> Children { get; set; } = null!;
+    public ICollection<Student> Children { get; set; } = null!;
 }
--- PSchool.DAL/Entities/Student.cs
+++ PSchool.DAL/Entities/Student.cs
@@ -7,5 +7,5 @@ public class Student : BaseEntity
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string Group { get; set; } = null!;
-    public IEnumerable<Parent> Parents { get; set; } = null!;
+    public ICollection<Parent> Parents { get; set; } = null!;
 }
